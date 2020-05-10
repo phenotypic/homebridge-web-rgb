de8c25f5a80d2a3f65e947243d7519bf590c8850
phenotypic <33377034+phenotypic@users.noreply.github.com>
2020-05-10 15:43:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -107,21 +107,21 @@ HTTP_RGB.prototype = {
         this.cacheHue = hsv[0]
         this.cacheSaturation = hsv[1]
         this.service.getCharacteristic(Characteristic.On).updateValue(json.currentState)
-        this.log('Updated state to: %s', json.currentState)
+        this.log.debug('Updated state to: %s', json.currentState)
         if (!this.disableBrightness) {
           this.service.getCharacteristic(Characteristic.Brightness).updateValue(json.currentBrightness)
-          this.log('Updated brightness to: %s', json.currentBrightness)
+          this.log.debug('Updated brightness to: %s', json.currentBrightness)
         }
         if (!this.disableColor) {
           this.service.getCharacteristic(Characteristic.Hue).updateValue(this.cacheHue)
           this.log.debug('Updated hue to: %s', this.cacheHue)
           this.service.getCharacteristic(Characteristic.Saturation).updateValue(this.cacheSaturation)
           this.log.debug('Updated saturation to: %s', this.cacheSaturation)
-          this.log('Updated color to: %s', json.currentColor)
+          this.log.debug('Updated color to: %s', json.currentColor)
         }
         if (this.colorTemperature) {
           this.service.getCharacteristic(Characteristic.ColorTemperature).updateValue(json.colorTemperature)
-          this.log('Updated color temperature to: %s', json.colorTemperature)
+          this.log.debug('Updated color temperature to: %s', json.colorTemperature)
         }
         callback()
       }
